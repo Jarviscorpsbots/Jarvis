2af2cad873c5d520908f9b0779befa6b8cfd3109
Jarviscorpsbots <159038420+Jarviscorpsbots@users.noreply.github.com>
2024-02-05 20:47:34 +0530

    Add files via upload

---
+++ Modules/source.py
@@ -0,0 +1,40 @@
+from platform import python_version as y
+
+from pyrogram import __version__ as z
+from pyrogram import filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
+from telegram import __version__ as o
+from telethon import __version__ as s
+
+from jarvis import BOT_NAME, BOT_USERNAME, OWNER_ID, START_IMG, pbot
+
+
+@pbot.on_message(filters.command(["duck", "tick"]))
+async def repo(_, message: Message):
+    await message.reply_photo(
+        photo=START_IMG,
+        caption=f"""**ʜᴇʏ {message.from_user.mention},
+
+ɪ ᴀᴍ [{BOT_NAME}](https://t.me/{BOT_USERNAME})**
+
+**» ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ :** jarvis
+**» ᴩʏᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{y()}`
+**» ʟɪʙʀᴀʀʏ ᴠᴇʀsɪᴏɴ :** `{o}` 
+**» ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{s}` 
+**» ᴘʏʀᴏɢʀᴀᴍ ᴠᴇʀsɪᴏɴ :** `{z}`
+""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton("ᴅᴇᴠᴇʟᴏᴘᴇʀ", user_id=OWNER_ID),
+                    InlineKeyboardButton(
+                        "sᴏᴜʀᴄᴇ",
+                        url="https://github.com/AnonymousX1025/jarvis",
+                    ),
+                ]
+            ]
+        ),
+    )
+
+
+__mod_name__ = "Rᴇᴩᴏ"
